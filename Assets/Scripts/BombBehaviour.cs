eb775efc93aa04e88fd796dc009be3c919f291f4
Excilyano <agahnim13@gmail.com>
2018-01-27 02:13:35 +0100

    Action drone

---
+++ Assets/Scripts/BombBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombBehaviour : Piege {
+
+	// Use this for initialization
+	void Start () {
+		Invoke("Explode", 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void Explode() {
+		Debug.Log("BOOM");
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/DroneAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneAction : MonoBehaviour {
+	public Piege TODELETE;
+
+	private List<Piege> pieges = new List<Piege>();
+	// Use this for initialization
+	void Start () {
+		pieges.Add(TODELETE);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 float action = Input.GetAxisRaw ("ActionP2");
+		 if (action != 0f && pieges.Count > 0 ) {
+			 Instantiate(pieges[0], transform.position, transform.rotation);
+			 pieges.RemoveAt(0);
+		 }
+	}
+}
